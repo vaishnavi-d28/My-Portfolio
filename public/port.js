5cd54817b5d0979a6b5d942893a36a444430baa2
vaishnavi-d28 <vaishnavid.connect@gmail.com>
2025-03-08 18:50:56 +0530

    Add files via upload

---
+++ public/port.js
@@ -0,0 +1,8 @@
+
+
+    var typed = new Typed('.auto-type', {
+        strings: ["Frontend Developer", "Web Developer"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
